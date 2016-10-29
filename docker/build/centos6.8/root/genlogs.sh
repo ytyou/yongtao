30839b703777e5eb769501547fa0e72245f4be3f
Yongtao You <yongtao_you@yahoo.com>
2016-10-29 15:51:25 -0700

    Use diff ways to generate random tags

--- docker/build/centos6.8/root/genlogs.sh
+++ docker/build/centos6.8/root/genlogs.sh
@@ -13,8 +13,8 @@ do
     timestamp=$(date)
     metric1=$(shuf -i 1-100 -n 1)
     metric2=$(shuf -i 1-10000 -n 1)
-    tag1=$(date | md5sum | head -c 8)
-    tag2=$(date | md5sum | head -c 8)
+    tag1=$(date | md5sum | head -c 4)
+    tag2=$(openssl rand -base64 32 | head -c 4)
     echo "${timestamp} metric1=${metric1} metric2=${metric2} tag1=\"${tag1}\" tag2=\"${tag2}\"" >> $LOGFILE
     sleep 1
 done
